a1f19bc2923ada9fa42efe486c27671081cc17b4
Stephan vd Elst <dev@stphn.cc>
2021-12-03 20:54:48 +0100

    added 7tv and fixed bttv

--- download.js
+++ download.js
@@ -3,56 +3,75 @@ const Path = require('path')
 const axios = require('axios');
 
 const baseUrlFFZ = "https://api.frankerfacez.com/v1/room/";
-const baseUrlBTTV = "https://api.betterttv.net/2/channels/";
+const baseUrlBTTV = "https://api.betterttv.net/3/cached/users/twitch/";
 const baseUrlTwitch = "https://api.twitch.tv/kraken/users?login="
+const baseUrlSevenTV = "https://api.7tv.app/v2/users/" // https://api.7tv.app/v2/users/id/emotes
 
-const BTTVEmote = "https://cdn.betterttv.net/emote/"
-axios.get(baseUrlFFZ + process.argv[2])
+const BTTVEmote = "https://cdn.betterttv.net/emote/" //https://cdn.betterttv.net/emote/id/3x
+const SevenTVEmote = "https://cdn.7tv.app/emote/" // https://cdn.7tv.app/emote/id/3x
+
+getFFZ(process.argv[2])
+getBTTV(process.argv[2])
+getSevenTV(process.argv[2])
+
+
+async function getFFZ(input) {
+    axios.get(baseUrlFFZ + input)
     .then(response => {
-        console.log("Getting FFZ Emotes of " + process.argv[2])
+        console.log(`Getting FFZ Emotes of ${input}`)
         Object.values(response.data.sets)[0].emoticons.forEach(e => {
-            // console.log(e.urls[Object.keys(e.urls)[Object.keys(e.urls).length - 1]])
             downloadEmote(process.argv[2], e.urls[Object.keys(e.urls)[Object.keys(e.urls).length - 1]].replace(/^.{2}/g, 'https://'), e.name,undefined, 'ffz')
         });
         
     })
     .catch(error => {
        if(error.status == 404){
-           console.log("\x1b[31m", "No FFZ User Found: Status 404")
+           console.log("No FFZ User Found: Status 404")
        }
-    })
-
- axios.get(baseUrlBTTV + process.argv[2])
- .then(response => {
-     if(response.data.status == 404){
-         console.log("\x1b[31m", "No BTTV User Found: Status 404")
-         return
-     }
-     console.log("\x1b[32m", "Getting BTTV Emotes of " + process.argv[2])
-     response.data.emotes.forEach(e => {
-         downloadEmote(process.argv[2], BTTVEmote + e.id + "/3x", e.code, "." + e.imageType, 'bttv')
-     })
- })
- .catch(error => {
-     if(error.response.status == 404){
-         console.log("\x1b[31m", "No FFZ User Found: Status 404")
-     }
- })
-
-axios.get(baseUrlTwitch + process.argv[2], {
-    headers: {
-        'client-id': 'r0kkkg5s7h1rdvxwehjose1w7ms7ro',
-        Accept: 'application/vnd.twitchtv.v5+json'
-    }
 })
+}
+
+async function getBTTV(input) {
+    let user = await axios.get(`https://api.ivr.fi/twitch/resolve/${input}`, {timeout: 10000});
+    console.log((`${baseUrlBTTV}${user.data.id}`))
+    axios.get(`${baseUrlBTTV}${user.data.id}`)
     .then(response => {
-        downloadTwitch(response.data['users'][0]._id)
+        if(response.data.status == 404){
+            console.log("No BTTV Emotes found Status 404")
+            return
+        }
+        console.log(`Getting BTTV Emotes of ${input}`)
+        response.data.channelEmotes.forEach(e => {
+            downloadEmote(process.argv[2], `${BTTVEmote}${e.id}/3x`, e.code, `.${e.imageType}`, 'bttv')
+        })
+        response.data.sharedEmotes.forEach(e => {
+            downloadEmote(process.argv[2], `${BTTVEmote}${e.id}/3x`, e.code, `.${e.imageType}`, 'bttv')
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+async function getSevenTV(input) {
+    let user = await axios.get(`https://api.ivr.fi/twitch/resolve/${input}`, {timeout: 10000});
+    axios.get(`${baseUrlSevenTV}${user.data.id}/emotes`)
+    .then(response => {
+        if(response.data.status == 404){
+            console.log(`No 7TV Emotes found for ${input} Status 404`)
+            return
+        }
+        console.log(`Getting 7TV Emotes of ${input}`)
+        response.data.forEach(e => {
+            downloadEmote(process.argv[2], SevenTVEmote + e.id + "/2x", e.name, ".webp", '7tv')
+        })
     })
     .catch(error => {
         if(error.response.status == 404){
-            console.log("\x1b[31m", "No FFZ User Found: Status 404")
+            console.log(`No 7TV Emotes found for ${input} Status 404`)
         }
     })
+}
 
 var fs = require('fs');
 var dir = process.argv[2];
@@ -74,26 +93,9 @@ if (!fs.existsSync('downloads/' + dir + '/bttv')){
     fs.mkdirSync('downloads/'+ dir + '/bttv');
 }
 
-if (!fs.existsSync('downloads/' + dir + '/twitch')){
-    fs.mkdirSync('downloads/' + dir + '/twitch');
+if (!fs.existsSync('downloads/' + dir + '/7tv')){
+    fs.mkdirSync('downloads/' + dir + '/7tv');
 }
-
-async function downloadTwitch(id){
-    axios.get("https://api.twitchemotes.com/api/v4/channels/" + id)
-        .then(response => {
-            response.data.emotes.forEach(e => {
-                downloadEmote(process.argv[2], `https://static-cdn.jtvnw.net/emoticons/v1/${e.id}/4.0`, e.code, '.png', 'twitch')
-            })
-        })
-        .catch(error => {
-            if (error.response.status == 404){
-                console.log("\x1b[31m", "User doens't have channel emotes")
-            }else{
-            // console.log(error)
-            }
-    })
-}
-
 
 async function downloadEmote (channel, url, name, imageType, thirdparty) {
     let type;
@@ -102,7 +104,7 @@ async function downloadEmote (channel, url, name, imageType, thirdparty) {
     }else {
         type = imageType
     }
-    const path = Path.resolve(__dirname + '/downloads', `${channel}/${thirdparty}`, name + type)
+    const path = Path.resolve( `${__dirname}/downloads`, `${channel}/${thirdparty}`, `${name}${type}`)
     const writer = Fs.createWriteStream(path)
   
     const response = await axios({
@@ -110,13 +112,12 @@ async function downloadEmote (channel, url, name, imageType, thirdparty) {
       method: 'GET',
       responseType: 'stream'
     }).catch(error => {
-        // console.log(error)
+        console.log(error)
     })
     response.data.pipe(writer)
   
     return new Promise((resolve, reject) => {
       writer.on('finish', resolve)
-    //   writer.on('error', reject)
     })
   }
 
